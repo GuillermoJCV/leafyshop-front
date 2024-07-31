2b6400c0b03d110a9c88c4b1df6b2b4cff2b476f
GuillermoJCV <memo92975@gmail.com>
2024-07-30 19:29:31 -0600

    Añade el template básico de astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import { nextui } from "@nextui-org/react"
+
+export default {
+	content: 	[
+				'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+				"./node_modules/@nextui-org/**/dist/*.{js,ts,jsx,tsx}"
+				],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [
+		nextui({
+			addCommonColors : true
+		})
+	],
+}
