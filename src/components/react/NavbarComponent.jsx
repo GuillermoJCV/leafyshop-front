174297ef071b9b76a3f891c3add0ba25d7526b93
Mariq12 <fmlabanda@utpl.edu.ec>
2024-08-01 14:45:40 -0500

    Cambios en el diseño del menu y creación de otra opción de navbar.jsx

--- src/components/react/NavbarComponent.jsx
+++ src/components/react/NavbarComponent.jsx
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './NavbarComponent.css';
 
 export default function NavbarComponent() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <Navbar isBordered>
-            <NavbarBrand>
-                <p className="font-bold text-inherit">Leafyshop</p>
-            </NavbarBrand>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Inicio
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link href="#" aria-current="page">
-                        Productos
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Contacto
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Sobre nosotros
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
+        <Navbar isBordered className="bg-gray-100">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <NavbarBrand>
+                    <img src="/src/assets/logo2.png" className="h-8" alt="Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Leafyshop</span>
+                </NavbarBrand>
+                <div className="flex md:order-2">
+                    <button
+                        onClick={handleMenuToggle}
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-default"
+                        aria-expanded={isMenuOpen}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        {isMenuOpen ? <FaTimes className="w-5 h-5" /> : <FaBars className="w-5 h-5" />}
+                    </button>
+                </div>
+                <div className={`w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'mt-4 block' : 'hidden'} md:flex md:mt-0`} id="navbar-default">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
+                                Inicio
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Productos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Contacto
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Sobre nosotros
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Button as={Link} color="primary" href="#" variant="flat" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Sign Up
+                            </Button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </Navbar>
     );
 }
