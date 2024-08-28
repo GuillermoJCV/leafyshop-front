634238594f005991de5924d909da7ca789379218
Mariq12 <fmlabanda@utpl.edu.ec>
2024-08-28 14:58:31 -0500

    Se agrega cloudflare al proyecto y actualización del README

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "hybrid",
+  adapter: cloudflare()
 });
